fddf058a90d0691e1fba6204d0d34bf533e98e3c
unknown <patryk2710dd@gmail.com>
2020-04-21 16:21:06 +0100

    Added printproducts

--- ie/tudublin/Cafe.java
+++ ie/tudublin/Cafe.java
@@ -11,11 +11,29 @@ import processing.data.TableRow;
 
 public class Cafe extends PApplet {
 
+    ArrayList<Product> products = new ArrayList<Product>();
+    ArrayList<Product> bill = new ArrayList<Product>();
+
+    public void loadData() {
+        Table table = loadTable("cafe.csv", "header");
+        for(TableRow row:table.rows()) {
+            Product p = new Product(row);
+            products.add(p);
+        }
+    }
+
+    public void printProducts() {
+        for(Product p:products) {
+            System.out.println(p);
+        }
+    }
+
     public void settings() {
         size(800,600);
     }
 
-    
+    public void setup() {
+        loadData();
+    }
 
 }
-
